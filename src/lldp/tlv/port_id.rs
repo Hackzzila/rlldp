88fb6b79cb873ee05f03eed3c6df4d8d92d8427d
Hackzzila <zach@hackzzila.com>
2024-04-26 15:33:33 -0400

    add missing port id subtypes

--- src/lldp/tlv/port_id.rs
+++ src/lldp/tlv/port_id.rs
@@ -2,7 +2,7 @@ use std::{borrow::Cow, cmp::Ordering};
 
 use crate::MacAddress;
 
-use super::TlvDecodeError;
+use super::{NetworkAddress, TlvDecodeError};
 
 pub enum PortIdKind {
   IfAlias,
@@ -46,22 +46,37 @@ impl From<PortIdKind> for u8 {
 
 #[derive(Debug, Clone)]
 pub enum PortId<'a> {
+  InterfaceAlias(Cow<'a, str>),
+  PortComponent(Cow<'a, str>),
   MacAddress(MacAddress),
+  NetworkAddress(NetworkAddress<'a>),
   InterfaceName(Cow<'a, str>),
+  AgentCircuitId(Cow<'a, [u8]>),
+  Local(Cow<'a, str>),
 }
 
 impl<'a> PortId<'a> {
   pub fn kind(&self) -> PortIdKind {
     match self {
+      Self::InterfaceAlias(_) => PortIdKind::IfAlias,
+      Self::PortComponent(_) => PortIdKind::Port,
       Self::MacAddress(_) => PortIdKind::LlAddr,
+      Self::NetworkAddress(_) => PortIdKind::Addr,
       Self::InterfaceName(_) => PortIdKind::IfName,
+      Self::AgentCircuitId(_) => PortIdKind::AgentCid,
+      Self::Local(_) => PortIdKind::Local,
     }
   }
 
   pub fn to_static(self) -> PortId<'static> {
     match self {
+      Self::InterfaceAlias(x) => PortId::InterfaceAlias(Cow::Owned(x.into_owned())),
+      Self::PortComponent(x) => PortId::PortComponent(Cow::Owned(x.into_owned())),
       Self::MacAddress(x) => PortId::MacAddress(x),
+      Self::NetworkAddress(x) => PortId::NetworkAddress(x.to_static()),
       Self::InterfaceName(x) => PortId::InterfaceName(Cow::Owned(x.into_owned())),
+      Self::AgentCircuitId(x) => PortId::AgentCircuitId(Cow::Owned(x.into_owned())),
+      Self::Local(x) => PortId::Local(Cow::Owned(x.into_owned())),
     }
   }
 
@@ -71,19 +86,25 @@ impl<'a> PortId<'a> {
     }
 
     let subtype = buf[0].try_into().map_err(TlvDecodeError::UnknownPortIdSubtype)?;
+    let buf = &buf[1..];
     match subtype {
-      PortIdKind::IfName => Ok(PortId::InterfaceName(String::from_utf8_lossy(buf[1..].into()))),
+      PortIdKind::IfName => Ok(PortId::InterfaceName(String::from_utf8_lossy(buf))),
+      PortIdKind::IfAlias => Ok(PortId::InterfaceAlias(String::from_utf8_lossy(buf))),
+      PortIdKind::Port => Ok(PortId::PortComponent(String::from_utf8_lossy(buf))),
+      PortIdKind::Local => Ok(PortId::Local(String::from_utf8_lossy(buf))),
 
-      PortIdKind::LlAddr => match buf.len().cmp(&7) {
+      PortIdKind::AgentCid => Ok(PortId::AgentCircuitId(Cow::Borrowed(buf))),
+
+      PortIdKind::Addr => Ok(PortId::NetworkAddress(NetworkAddress::parse(buf)?)),
+
+      PortIdKind::LlAddr => match buf.len().cmp(&6) {
         Ordering::Less => Err(TlvDecodeError::BufferTooShort),
         Ordering::Greater => Err(TlvDecodeError::BufferTooLong),
         Ordering::Equal => {
-          let mac = buf[1..7].try_into().unwrap();
+          let mac = buf[0..6].try_into().unwrap();
           Ok(PortId::MacAddress(MacAddress(mac)))
         }
       },
-
-      x => Err(TlvDecodeError::UnknownPortIdSubtype(x.into())),
     }
   }
 }
